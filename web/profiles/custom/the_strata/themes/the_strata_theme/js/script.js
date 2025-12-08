3acf054d7e3356dcd1c6dbe2a410179b8deb5603
Tiago Andrade <tiago.andrade@onemednet.com>
2025-12-07 18:58:11 -0800

    New the_strata_theme

---
+++ web/profiles/custom/the_strata/themes/the_strata_theme/js/script.js
@@ -0,0 +1,15 @@
+/**
+ * @file
+ * The Strata Theme - Custom JavaScript.
+ */
+
+(function (Drupal) {
+  'use strict';
+
+  Drupal.behaviors.theStrataTheme = {
+    attach: function (context, settings) {
+      // Add custom JavaScript here.
+    }
+  };
+
+})(Drupal);
